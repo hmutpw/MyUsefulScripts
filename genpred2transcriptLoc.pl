603505302bcb7a7d03fcc3d11032b05d7b55a456
Puwen Tan <37172600+hmutpw@users.noreply.github.com>
2024-10-11 13:26:26 -0400

    Add files via upload

--- genpred2transcriptLoc.pl
+++ genpred2transcriptLoc.pl
@@ -3,21 +3,25 @@ use strict;
 use warnings;
 use Getopt::Long;
 
+# ????
 my $input_file;
 my $output_file;
+my $genePredExt = 0;  # ????? genePredExt ??
 
+# ???????
 GetOptions(
-    'output=s' => \$output_file,
-    'help'     => sub { print_usage(); exit; },
+    'output=s'      => \$output_file,
+    'genePredExt'   => \$genePredExt,
+    'help'          => sub { print_usage(); exit; },
 ) or die "Invalid options passed. Use --help for usage.\n";
 
-# check the input
-$input_file = shift or die "Usage: $0 [--output OUTPUT] INPUT\n";
+# ??????
+$input_file = shift or die "Usage: $0 [--output OUTPUT] [--genePredExt] INPUT\n";
 
-# open genPred
+# ?? genePred ? genePredExt ??
 open(my $genpred_fh, '<', $input_file) or die "Could not open $input_file: $!";
 
-# open output or STD
+# ????????? STDOUT
 my $output_fh;
 if (defined $output_file) {
     open($output_fh, '>', $output_file) or die "Could not open $output_file: $!";
@@ -25,38 +29,110 @@ if (defined $output_file) {
     $output_fh = \*STDOUT;
 }
 
-# output header
-print $output_fh join("\t", "gene_id", "tx_id", "chr", "start", "strand", "genomic_pos", "tx_pos", "exon_number", "total_exons"), "\n";
+# ????
+print $output_fh join("\t", "gene_id", "tx_id", "chr", "genomic_pos", "strand", "genomic_pos_strand", "tx_pos", "exon_number", "total_exons"), "\n";
 
+# ?????
 while (my $line = <$genpred_fh>) {
     chomp $line;
     my @fields = split "\t", $line;
 
-    # get information from genpred
-    my $gene_id = $fields[0];
-    my $transcript_id = $fields[1];
-    my $chromosome = $fields[2];
-    my $strand = $fields[3];
-    my @exon_starts = split ',', $fields[9];
-    my @exon_ends = split ',', $fields[10];
+    # ??????
+    if ($genePredExt) {
+        if (@fields < 15) {
+            warn "Skipping line with insufficient fields for genePredExt: $line\n";
+            next;
+        }
+    } else {
+        if (@fields < 10) {
+            warn "Skipping line with insufficient fields for genePred: $line\n";
+            next;
+        }
+    }
 
-    # remove null element
-    pop @exon_starts if $exon_starts[-1] eq '';
-    pop @exon_ends if $exon_ends[-1] eq '';
+    # ????
+    my ($gene_id, $transcript_id, $chromosome, $strand, @exon_starts, @exon_ends);
+
+    if ($genePredExt) {
+        $transcript_id = $fields[0];
+        $chromosome    = $fields[1];
+        $strand        = $fields[2];
+        # ?? genePredExt ?????????
+        @exon_starts = split ',', $fields[8];
+        @exon_ends   = split ',', $fields[9];
+        $gene_id      = $fields[11];
+    } else {
+        $transcript_id = $fields[0];
+        $chromosome    = $fields[1];
+        $strand        = $fields[2];
+        # ?? genePred ?????????
+        @exon_starts = split ',', $fields[8];
+        @exon_ends   = split ',', $fields[9];
+        $gene_id      = $fields[0];  # ?? genePred ? gene_id ? transcript_id ??,?????
+    }
+
+    # ????????(???)
+    pop @exon_starts if defined $exon_starts[-1] && $exon_starts[-1] eq '';
+    pop @exon_ends if defined $exon_ends[-1] && $exon_ends[-1] eq '';
+
+    # ?? exon_starts ? exon_ends ????
+    if (@exon_starts != @exon_ends) {
+        warn "Mismatch between exon starts and ends in line: $line\n";
+        next;
+    }
 
     my $transcript_position = 1;
+    my $total_exons = scalar @exon_starts;
+
     if ($strand eq '+') {
-        for (my $i = 0; $i < scalar @exon_starts; $i++) {
+        for (my $i = 0; $i < $total_exons; $i++) {
             my $exon_start = $exon_starts[$i];
-            my $exon_end = $exon_ends[$i];
+            my $exon_end   = $exon_ends[$i];
             my $exon_length = $exon_end - $exon_start;
 
-            for (my $j = 0; $j < $exon_length; $j++) {
-                my $genomic_position = $exon_start + $j + 1;  # Adjust to 1-based position
-                my $exon_number = $i + 1;
-                my $total_exons = scalar @exon_starts;
+            # ?? exon_length ??
+            if ($exon_length <= 0) {
+                warn "Invalid exon length in line: $line\n";
+                next;
+            }
 
-                # print basepair information
+            for (my $j = 0; $j < $exon_length; $j++) {
+                my $genomic_position = $exon_start + $j + 1;  # ???1-based??
+                my $exon_number = $i + 1;
+
+                # ???????
+                print $output_fh join("\t",
+                    $gene_id,
+                    $transcript_id,
+                    $chromosome,
+                    $genomic_position,
+                    $strand,
+                    "$chromosome:$genomic_position:$strand",
+                    $transcript_position,
+                    $exon_number,
+                    $total_exons
+                ), "\n";
+
+                $transcript_position++;
+            }
+        }
+    } elsif ($strand eq '-') {
+        for (my $i = $total_exons - 1; $i >= 0; $i--) {
+            my $exon_start = $exon_starts[$i];
+            my $exon_end   = $exon_ends[$i];
+            my $exon_length = $exon_end - $exon_start;
+
+            # ?? exon_length ??
+            if ($exon_length <= 0) {
+                warn "Invalid exon length in line: $line\n";
+                next;
+            }
+
+            for (my $j = $exon_length - 1; $j >= 0; $j--) {
+                my $genomic_position = $exon_start + $j + 1;  # ???1-based??
+                my $exon_number = $i + 1;
+
+                # ???????
                 print $output_fh join("\t",
                     $gene_id,
                     $transcript_id,
@@ -73,56 +149,35 @@ while (my $line = <$genpred_fh>) {
             }
         }
     } else {
-        for (my $i = scalar @exon_starts - 1; $i >= 0; $i--) {
-            my $exon_start = $exon_starts[$i];
-            my $exon_end = $exon_ends[$i];
-            my $exon_length = $exon_end - $exon_start;
-
-            for (my $j = $exon_length - 1; $j >= 0; $j--) {
-                my $genomic_position = $exon_start + $j + 1;  # Adjust to 1-based position
-                my $exon_number = $i + 1;
-                my $total_exons = scalar @exon_starts;
-
-                # print basepair information
-                print $output_fh join("\t",
-                    $gene_id,
-                    $transcript_id,
-                    $chromosome,
-                    $genomic_position,
-                    $strand,
-                    "$chromosome:$genomic_position:$strand",
-                    $transcript_position,
-                    $exon_number,
-                    $total_exons
-                ), "\n";
-
-                $transcript_position++;
-            }
-        }
+        warn "Unknown strand '$strand' in line: $line\n";
     }
 }
 
+# ??????
 close($genpred_fh);
 close($output_fh) if defined $output_file;
 
+# ????
 sub print_usage {
     print << "USAGE";
-Usage: $0 [--output OUTPUT] INPUT
+Usage: $0 [--output OUTPUT] [--genePredExt] INPUT
 
 Description:
-    This script processes a genpred file and outputs the annotation of each basepair
+    This script processes a genePred or genePredExt file and outputs the annotation of each basepair
     in each transcript. The output includes gene id, transcript id, chromosome,
     strand, genomic position, transcript position, exon number, and total exons.
     
-    INPUT is the path to the input genpred file.
+    INPUT is the path to the input genePred or genePredExt file.
     --output OUTPUT is the path to the output file. If not provided, the output will be
     written to standard output.
-
+    --genePredExt    Specify that the input file is in genePredExt format.
+    
 Options:
     --help      Show this help message and exit.
 
 Example:
     perl $0 --output output.txt input.gpd
+    perl $0 --genePredExt --output output.txt input.gpdExt
     perl $0 input.gpd > output.txt
 
 USAGE
